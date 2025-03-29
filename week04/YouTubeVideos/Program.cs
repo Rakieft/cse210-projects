cba19e10792936ebf643a3d5dbf5a7a0c9123471
Rakieft <kieftraphter.joly1@gmail.com>
2025-03-28 23:42:26 -0400

    week 4

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,21 +10,21 @@ class Program
         List<Video> videos = new List<Video>();
 
         // Creating the first video
-        Video video1 = new Video("C# Tutorial for Beginners", "John Doe", 600);
+        Video video1 = new Video("C# Tutorial for Beginners", "Jean Jr Dorestant", 600);
         video1.AddComment(new Comment("Clervil", "Great tutorial"));
         video1.AddComment(new Comment("Quelitho", "Very helpful, thanks"));
         video1.AddComment(new Comment("Charlie", "I learned a lot."));
         videos.Add(video1);
 
         // Creating the second video
-        Video video2 = new Video("How to Cook Pasta", "Chef Marie", 450);
+        Video video2 = new Video("How to Cook Pasta", "Chef Marie Vierge", 450);
         video2.AddComment(new Comment("David", "This recipe is amazing"));
         video2.AddComment(new Comment("Moise", "Simple and delicious."));
         video2.AddComment(new Comment("Dorestant", "Can you share more recipes?"));
         videos.Add(video2);
 
         // Creating the third video
-        Video video3 = new Video("Top 10 Travel Destinations", "AdventureWorld", 720);
+        Video video3 = new Video("Top 10 Travel Destinations", "T-machan", 720);
         video3.AddComment(new Comment("George", "Adding these to my bucket list"));
         video3.AddComment(new Comment("Hanna", "Love the visuals"));
         video3.AddComment(new Comment("Isaac", "Amazing places"));
